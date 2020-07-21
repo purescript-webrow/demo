e3350cee24af4f76193f164b8d7baa1ab291b1ee
Tomasz Rybarczyk <paluho@gmail.com>
2020-07-21 15:44:07 +0200

    Handle messaging using testing defaults

--- src/Main.purs
+++ src/Main.purs
@@ -8,7 +8,7 @@ import Data.Map (insert, lookup) as Map
 import Data.Maybe (Maybe(..), isJust)
 import Data.Tuple (snd) as Tuple
 import Data.Variant (Variant, case_, on)
-import Data.Variant (inj) as Variant
+import Data.Variant (inj, match) as Variant
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Random (random)
@@ -16,38 +16,50 @@ import Effect.Ref (Ref)
 import Effect.Ref (modify_, new, read) as Ref
 import HTTPure (Method(..), Request, ResponseM)
 import HTTPure (serve) as HTTPure
+import Polyform.Batteries.Messages (string, urlEncoded) as Batteries.Messages
+import Polyform.Batteries.Messages.String (Messages) as String
+import Polyform.Batteries.Messages.UrlEncoded (Messages) as Batteries.Messages.UrlEncoded
+import Polyform.Batteries.Messages.UrlEncoded (Messages) as UrlEncoded
 import Record.Builder (build, insert) as Record.Builder
 import Routing.Duplex (RouteDuplex', root) as D
 import Routing.Duplex.Generic (noArgs, sum) as D
 import Routing.Duplex.Generic.Variant (variant') as RouteDuplex.Variant
 import Run (Run, runBaseAff')
-import Run (liftEffect, on, run, send) as Run
+import Run (interpret, liftEffect, on, run, send) as Run
 import Text.Smolder.HTML (a, li, ul) as M
 import Text.Smolder.HTML.Attributes (href) as A
 import Text.Smolder.Markup ((!))
 import Text.Smolder.Markup (text) as M
 import Type.Prelude (SProxy(..))
 import Type.Row (type (+))
+import Unsafe.Coerce (unsafeCoerce)
 import WebRow (method, ok) as W
-import WebRow.Applets.Auth (Route(..), RouteRow, routeBuilder, router) as Auth
+import WebRow.Applets.Auth (Route(..), RouteRow, Messages, routeBuilder, router) as Auth
 import WebRow.Applets.Auth.Effects (AuthF(..))
+import WebRow.Applets.Auth.Testing.Messages (auth) as Auth.Testing.Messages
 import WebRow.Applets.Auth.Testing.Templates (render) as Auth.Testing.Templates
 import WebRow.Applets.Auth.Types (Password, _auth)
-import WebRow.Applets.Registration (Route(..), RouteRow, routeBuilder) as Registration
+import WebRow.Applets.Registration (Route(..), RouteRow, Messages, routeBuilder) as Registration
+import WebRow.Applets.Registration (_emailVerification, registerEmail)
 import WebRow.Applets.Registration (router) as Registarion
 import WebRow.Applets.Registration.Effects (Registration, RegistrationF(..))
+import WebRow.Applets.Registration.Testing.Messages (registration) as Registration.Testing.Messages
 import WebRow.Applets.Registration.Testing.Templates (render) as Registration.Testing.Templates
 import WebRow.Applets.Registration.Types (_registration)
 import WebRow.Contrib.Run (EffRow)
 import WebRow.Crypto (run) as Crypto
+import WebRow.Forms.Validators (Messages) as WebRow.Forms.Validators
 import WebRow.HTTP.Cookies (run) as Cookies
 import WebRow.HTTP.Response (ok, run) as Response
 import WebRow.HTTP.Response (setHeader)
 import WebRow.KeyValueStore.Types (Key)
-import WebRow.Mailer (Email)
-import WebRow.Routing (RelativeUrl(..), printRoute, route, runRouting)
+import WebRow.Mailer (Email(..), Mailer, _mailer)
+import WebRow.Mailer (MailerF(..)) as Mailer
+import WebRow.Message (Message, MessageF(..), _message)
+import WebRow.Routing (FullUrl(..), RelativeUrl(..), printRoute, route, runRouting)
 import WebRow.Session (runInMemory) as Session
 import WebRow.Testing.Interpret (runMailer', runMessage) as Testing.Interpret
+import WebRow.Testing.Messages (validators) as WebRow.Testing.Messages
 import WebRow.Testing.Templates (html)
 
 data Root = Root
@@ -63,20 +75,12 @@ _root = SProxy ∷ SProxy ""
 
 routeDuplex ∷ D.RouteDuplex' Routes
 routeDuplex = D.root $ RouteDuplex.Variant.variant' $ Record.Builder.build
-    (Auth.routeBuilder <<< Registration.routeBuilder <<< Record.Builder.insert _root rootRouteDuplex) {}
-
-string s = do
-  x ← show <$> Run.liftEffect random
-  W.method >>= case _ of
-    Get → do
-      setHeader "Set-Cookie" "x=8"
-      W.ok x
-    Post → W.ok ("POST" <> x)
-    _ → W.ok ("Other" <> x)
+  ( Record.Builder.insert _root rootRouteDuplex
+  <<< Auth.routeBuilder
+  <<< Registration.routeBuilder
+  ) {}
 
 router = case_
-  # Auth.router
-  # Registarion.router
   # on _root
     ( \_ → pure $ Variant.inj _root do
         RelativeUrl loginUrl ← printRoute (Variant.inj _auth Auth.Login)
@@ -86,6 +90,8 @@ router = case_
             M.li $ M.a ! A.href registerUrl $ M.text "register"
             M.li $ M.a ! A.href loginUrl $ M.text "login"
     )
+  # Auth.router
+  # Registarion.router
 
 -- runAuth ∷ ∀ eff. Run (Auth () + eff) ~> Run eff
 runAuth db = Run.run (Run.on _auth handler Run.send)
@@ -106,7 +112,48 @@ runRegistration db = Run.run (Run.on _registration handler Run.send)
       Run.liftEffect (Ref.modify_ (Map.insert email password) db)
       pure next
 
+runMailer ∷ ∀ eff. Run (Mailer (emailVerification ∷ FullUrl) + EffRow + eff) ~> Run (EffRow + eff)
+runMailer = Run.run (Run.on _mailer handler Run.send)
+  where
+    sendToConsole { to, body, subject } = do
+      Run.liftEffect $ log $ "\nrecipient: " <> to
+      Run.liftEffect $ log $ "subject: " <> subject
+      Run.liftEffect $ log $ "body:\n" <> body <> "\n\n"
+
+    sendConfirmation to (FullUrl registrationLink) = sendToConsole
+      { body: "registration link:" <> registrationLink
+      , subject: "registration email"
+      , to
+      }
+
+    handler (Mailer.SendF { to: Email to, context } next) = do
+      case_ # on _emailVerification (sendConfirmation to) $ context
+      pure next
+
 type SessionData = { user ∷ Maybe { email ∷ Email }}
+
+type Messages =
+  ( Registration.Messages + Auth.Messages
+  + UrlEncoded.Messages + WebRow.Forms.Validators.Messages
+  + String.Messages
+  + ()
+  )
+
+handleMessage ∷ ∀ m msgs. Monad m ⇒ MessageF (Variant Messages) ~> m
+handleMessage (MessageF v next) = pure $ next (print v)
+  where
+    print = Variant.match printers
+    printers = Record.Builder.build
+      ( Registration.Testing.Messages.registration
+      <<< Auth.Testing.Messages.auth
+      -- | Rename this to validators?
+      <<< WebRow.Testing.Messages.validators
+      <<< Batteries.Messages.string
+      <<< Batteries.Messages.urlEncoded
+      )
+      {}
+
+runMessage = Run.interpret (Run.on _message handleMessage Run.send)
 
 app
   ∷ { user ∷ Ref (Map Email Password)
@@ -121,8 +168,8 @@ app db req = do
     <<< Crypto.run "seceret"
     <<< Cookies.run
     <<< Session.runInMemory db.session { user: Nothing }
-    <<< Testing.Interpret.runMessage
-    <<< (Testing.Interpret.runMailer' <#> map Tuple.snd)
+    <<< runMessage
+    <<< runMailer
     <<< runRegistration db.user
     <<< runAuth db.user $ do
       bind route $ router >=> (case_
